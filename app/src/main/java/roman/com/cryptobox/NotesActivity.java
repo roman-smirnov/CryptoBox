367481817c425a4b57573f30ab1ebdfc858189d8
Roman <bioelectromecha@gmail.com>
2016-09-10 19:19:43 +0300

    file example
    project created

---
+++ app/src/main/java/roman/com/cryptobox/NotesActivity.java
@@ -0,0 +1,14 @@
+package roman.com.cryptobox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+
+    }
+}
