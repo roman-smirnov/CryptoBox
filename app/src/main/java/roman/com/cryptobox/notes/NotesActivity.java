abb559cc881ae05e0982aa16a405f377537e9e4e
Roman <bioelectromecha@gmail.com>
2016-10-02 14:40:52 +0300

    R.S - added a nice fab with a click listener

--- app/src/main/java/roman/com/cryptobox/notes/NotesActivity.java
+++ app/src/main/java/roman/com/cryptobox/notes/NotesActivity.java
@@ -1,5 +1,7 @@
 package roman.com.cryptobox.notes;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -19,6 +21,7 @@ public class NotesActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private List<Note> mNoteList = new ArrayList<>();
     private NotesAdapter mNotesAdapter;
+    private FloatingActionButton mFloatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,16 @@ public class NotesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_notes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.notes_toolbar);
         setSupportActionBar(toolbar);
+
+        mFloatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+
+        mFloatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Click action
+                Toast.makeText(NotesActivity.this, "FAB CLICKED", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
