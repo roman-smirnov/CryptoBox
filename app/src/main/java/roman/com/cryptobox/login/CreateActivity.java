312a01dd2ebe88d4fcace647bd6cffb64b5540f2
Roman <bioelectromecha@gmail.com>
2016-10-04 16:33:01 +0300

    R.S  - removed databinding lib

--- app/src/main/java/roman/com/cryptobox/login/CreateActivity.java
+++ app/src/main/java/roman/com/cryptobox/login/CreateActivity.java
@@ -2,9 +2,15 @@ package roman.com.cryptobox.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.apkfuns.logutils.LogUtils;
 import com.nulabinc.zxcvbn.Strength;
 import com.nulabinc.zxcvbn.Zxcvbn;
 
@@ -13,14 +19,47 @@ import roman.com.cryptobox.R;
 
 public class CreateActivity extends AppCompatActivity {
     private Button mCreateButton;
+    private EditText mPasswordEditText;
+    private TextView mPasswordStrengthTextView;
+    private ProgressBar mPasswordStrengthProgressbar;
+    private Zxcvbn mZxcvbn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        mZxcvbn = new Zxcvbn();
+        mCreateButton = (Button) findViewById(R.id.button_create_password);
+        mPasswordEditText = (EditText) findViewById(R.id.edittext_password_input1);
+        mPasswordStrengthTextView = (TextView) findViewById(R.id.textview_password_strength);
+        mPasswordStrengthProgressbar = (ProgressBar) findViewById(R.id.progressbar_password_strength);
 
-        Zxcvbn zxcvbn = new Zxcvbn();
-        Strength strength = zxcvbn.measure("12345");
-        LogUtils.d(strength.getCrackTimesDisplay().getOfflineFastHashing1e10PerSecond());
+        mPasswordStrengthTextView.setText(getResources().getString(R.string.password_strength, " "));
+
+        mPasswordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Strength strength = mZxcvbn.measure(mPasswordEditText.getText().toString());
+                mPasswordStrengthTextView.setText(getResources().getString(R.string.password_strength, strength.getCrackTimesDisplay().getOfflineFastHashing1e10PerSecond()));
+            }
+        });
+
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(CreateActivity.this, "account created", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
