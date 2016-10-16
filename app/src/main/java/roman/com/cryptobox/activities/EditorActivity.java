de95624dcdb9fd9c890a12a8fe6506a3b9905ce3
Roman <bioelectromecha@gmail.com>
2016-10-16 17:51:06 +0300

    R.S - completed note editor activity

--- app/src/main/java/roman/com/cryptobox/activities/EditorActivity.java
+++ app/src/main/java/roman/com/cryptobox/activities/EditorActivity.java
@@ -1,19 +1,30 @@
 package roman.com.cryptobox.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
+import hugo.weaving.DebugLog;
 import roman.com.cryptobox.R;
 import roman.com.cryptobox.dataobjects.MockNote;
 
 public class EditorActivity extends AppCompatActivity {
 
+    private boolean mIsInEditingMode = false;
+
     private EditText mDateEditText;
     private EditText mTitleEditText;
     private EditText mContentEditText;
+    private MenuItem mMenuItem;
     private MockNote mNote;
 
     @Override
@@ -25,14 +36,13 @@ public class EditorActivity extends AppCompatActivity {
         Intent intent = getIntent();
         mNote = intent.getExtras().getParcelable(MockNote.NOTE_KEY_STRING);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.activity_editor_toolbar);
         setSupportActionBar(toolbar);
 
         //get the views
         mDateEditText = (EditText) findViewById(R.id.activity_editor_note_date);
         mTitleEditText = (EditText) findViewById(R.id.activity_editor_note_title);
         mContentEditText = (EditText) findViewById(R.id.activity_editor_note_content);
-        mDateEditText.setText(mNote.getLastModified());
 
         //set the text to the views
         mDateEditText.setText(mNote.getLastModified());
@@ -49,5 +59,77 @@ public class EditorActivity extends AppCompatActivity {
         mNote.setLastModified(mDateEditText.getText().toString());
         mNote.setTitle(mTitleEditText.getText().toString());
         mNote.setContent(mContentEditText.getText().toString());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_editor_menu, menu);
+        mMenuItem = menu.findItem(R.id.activity_editor_edit_icon);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (!mIsInEditingMode) {
+            startEdit();
+        } else {
+            finishEdit();
+        }
+        return true;
+    }
+
+    public void onClickDate(View view) {
+        //do nothing
+    }
+
+    public void onClickTitle(View view) {
+        if (!mIsInEditingMode) {
+            startEdit();
+            mTitleEditText.requestFocus();
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.showSoftInput(mTitleEditText, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    public void onClickContent(View view) {
+        if (!mIsInEditingMode) {
+            startEdit();
+            mContentEditText.requestFocus();
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.showSoftInput(mContentEditText, InputMethodManager.SHOW_IMPLICIT);
+        }
+    }
+
+    private void startEdit() {
+        //set title stuff
+        mTitleEditText.setFocusableInTouchMode(true);
+        mTitleEditText.setFocusable(true);
+        mTitleEditText.setBackground(getResources().getDrawable(R.drawable.bg_edit_text, getTheme()));
+        //set content stuff
+        mContentEditText.setFocusableInTouchMode(true);
+        mContentEditText.setFocusable(true);
+        mContentEditText.setBackground(getResources().getDrawable(R.drawable.bg_edit_text, getTheme()));
+
+        mIsInEditingMode = true;
+        mMenuItem.setIcon(getResources().getDrawable(R.drawable.ic_done, getTheme()));
+    }
+
+    @DebugLog
+    private void finishEdit() {
+        View currentFocus = getCurrentFocus();
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (currentFocus != null) {
+            inputMethodManager.hideSoftInputFromWindow(currentFocus.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+        //set title stuff
+        mTitleEditText.setFocusable(false);
+        mTitleEditText.setBackgroundColor(Color.TRANSPARENT);
+        //set content stuff
+        mContentEditText.setFocusable(false);
+        mContentEditText.setBackgroundColor(Color.TRANSPARENT);
+        mIsInEditingMode = false;
+        mMenuItem.setIcon(getResources().getDrawable(R.drawable.ic_edit, getTheme()));
+
     }
 }
