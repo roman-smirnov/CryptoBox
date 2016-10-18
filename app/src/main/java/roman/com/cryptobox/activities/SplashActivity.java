05a942a05ba9aad437be488f1d04cf07015995f6
Roman <bioelectromecha@gmail.com>
2016-10-18 19:31:06 +0300

    R.S  -  converted the Splash screen to mvp

--- app/src/main/java/roman/com/cryptobox/activities/SplashActivity.java
+++ app/src/main/java/roman/com/cryptobox/activities/SplashActivity.java
@@ -7,17 +7,19 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import roman.com.cryptobox.R;
+import roman.com.cryptobox.contracts.SplashContract;
 import roman.com.cryptobox.dataobjects.ApiTester;
+import roman.com.cryptobox.presenters.SplashPresenter;
+import roman.com.cryptobox.utils.MyApplication;
 
 /**
  * Launcher activity
  * This activity is the first one to be opened when the app is launched
  * It exists solely to display the EchoPark logo while the app is loading
  */
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements SplashContract.View {
 
-    private final static String RUN_NUMBER = "RUN_NUMBER";
-    private SharedPreferences mSharedPreferences;
+    private SplashContract.Presenter mPresenter;
 
     /* the logo is displayed through the theme-drawable (check out the manifest + stles + layout file )*/
     @Override
@@ -25,49 +27,35 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //decide what activity to load based on app run counter
-        mSharedPreferences = getSharedPreferences("cryptobox", Context.MODE_PRIVATE);
+        mPresenter = new SplashPresenter(this);
+        mPresenter.appLaunched();
 
-
-
-        ApiTester tester = new ApiTester();
-        tester.runScripts();
-
-        if (isFirstRun()) {
-            incrementRunCounter();
-            goToExplainActivity();
-        } else {
-            incrementRunCounter();
-            goToLoginActivity();
-        }
-    }
-
-
-    private boolean isFirstRun() {
-        return mSharedPreferences.getInt(RUN_NUMBER, 0) == 0;
-    }
-
-    private void incrementRunCounter() {
-        mSharedPreferences.edit().putInt(RUN_NUMBER, mSharedPreferences.getInt(RUN_NUMBER, 0) + 1).apply();
-    }
-
-    private void goToExplainActivity() {
-        //launch the permissions activity
-        Intent intent = new Intent(this, ExplainActivity.class);
-        startActivity(intent);
-        //kill the activity to prevent user from going back to the splash screen
-        finish();
+//        avishi's stuff
+//        ApiTester tester = new ApiTester();
+//        tester.runScripts();
     }
 
     /**
-     * this one will open the LoginActivity and prevent from going back to splash screen
+     * open the LoginActivity and prevent from going back to splash screen
      */
-    private void goToLoginActivity() {
-        //launch the permissions activity
+    @Override
+    public void showLoginActivity() {
+        //launch the login activity
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         //kill the activity to prevent user from going back to the splash screen
         finish();
     }
 
+    /**
+     * open the ExplainActivity and prevent from going back to splash screen
+     */
+    @Override
+    public void showExplainActivity() {
+        //launch the Explain activity
+        Intent intent = new Intent(this, ExplainActivity.class);
+        startActivity(intent);
+        //kill the activity to prevent user from going back to the splash screen
+        finish();
+    }
 }
