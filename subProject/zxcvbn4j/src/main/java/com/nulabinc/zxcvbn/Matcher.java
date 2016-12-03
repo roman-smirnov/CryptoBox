117677d4909f8ecb26799ef0cddd44d7420180a7
Roman <bioelectromecha@gmail.com>
2016-12-03 14:13:09 +0200

    R.S  - deleted the subproject

--- subProject/zxcvbn4j/src/main/java/com/nulabinc/zxcvbn/Matcher.java
+++
@@ -1,9 +0,0 @@
-package com.nulabinc.zxcvbn;
-
-import com.nulabinc.zxcvbn.matchers.Match;
-
-import java.util.List;
-
-public interface Matcher {
-    public List<Match> execute(String password);
-}
