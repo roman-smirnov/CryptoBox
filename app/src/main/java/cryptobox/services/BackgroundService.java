0dab2a8bc5729e189404974e775ffe81ed8f7491
Roman <bioelectromecha@gmail.com>
2016-12-17 18:46:22 +0200

    R.S -  created a service

---
+++ app/src/main/java/cryptobox/services/BackgroundService.java
@@ -0,0 +1,17 @@
+package cryptobox.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BackgroundService extends Service {
+    public BackgroundService() {
+
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
